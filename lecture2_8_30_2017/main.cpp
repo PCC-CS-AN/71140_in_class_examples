0c443a67310b34a1f90b46a407081be7872e92d9
Display <disp@Evelyn.local>
2017-09-02 20:17:36 -0700

    Added lecture 2 with example format

--- lecture2_8_30_2017/main.cpp
+++ lecture2_8_30_2017/main.cpp
@@ -1,9 +1,46 @@
+/*
+ * Assignment Title
+ * Full Name - ID Numbers
+ *
+ * Short problem description
+ *
+ * Inputs:
+ *     type name - description
+ *
+ * Outputs:
+ *     type name - description
+ *
+ */
+
 #include <iostream>
+
+// Required for the `string` type
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main() {
+  // Variable Declarations
+  int a;
+  string b;
+  double c;
+
+  // Statements
+  cout << "prompt" << endl;
+
+  // Working with strings
+  cin >> b;
+  cout << b << endl;
+
+  // integer division
+  a = 5;
+  cout << 6 / a << endl;
+
+  // doubles
+  c = 5.0;
+  cout << 6 / c << endl;
+
+
+  // Exit Program
+  return 0;
 }
