5465f1e6733692834616627cfae9f5ae686d1bb4
thepudding <alex@small-affair.com>
2017-09-20 17:28:48 -0700

    template

---
+++ other/template.cpp
@@ -0,0 +1,50 @@
+/**
+ * 
+ * Standard Header
+ *
+ **/
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+/**
+ * Function Declarations
+ **/
+
+void test();
+void run();
+
+/**
+ * Main Function
+ **/
+
+int main() {
+  char t;
+
+  cout << "Enter [t] to test, or any other character to run." << endl;
+  cin >> t;
+  if(t == 't') {
+    test();
+  } else {
+    run();
+  }
+}
+
+/**
+ * Function Definitions
+ **/
+
+void test() {
+
+}
+
+void run() {
+  // Variable Declarations
+
+  // Input
+
+  // Output
+
+}
