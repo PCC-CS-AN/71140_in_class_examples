9d2b520943c2eaa923b9470804b57ddfd0277676
display <display@Evelyn.local>
2017-10-18 19:45:38 -0700

    connect four example

---
+++ lecture_10_18_2017.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+using Row = vector<int>;
+using Board = vector<Row>;
+// Checks a single column for a win
+bool connect_col(Board b, int col) {
+	int i = 0;
+	
+	while(i < b.size() - 3) {
+		if( b[i][col] == b[i+1][col] && 
+			b[i+1][col] == b[i+2][col] && 
+			b[i+2][col] == b[i+3][col] ) {
+			return true;	
+		}
+		i = i + 1;
+	}
+	return false;
+}
+bool connect_vert(Board b) {
+	int i = 0;
+	while(i < b[0].size()) {
+		if(connect_col(b, i)){
+			return true;
+		}
+		i = i + 1;
+	}
+	return false;
+}
+// checks a single set of four spaces for a forward diag connect four
+// Assume valid input
+bool connect_single_fd(Board b, int row, int col) {
+	return b[row][col]     == b[row+1][col-1] &&
+		   b[row+1][col-1] == b[row+2][col-2] &&
+		   b[row+2][col-2] == b[row+3][col-3];
+}
+
+bool connect_fd_row(Board b, int row) {
+	int i = 3;
+	while(i < b.size()) {
+		if(connect_single_fd(b, row, i)){
+			return true;
+		}
+		i = i + 1;
+	}
+	return false;
+}
+
+bool connect_forward_diag(Board b) {
+	int i = 0;
+	
+	while(i < 3) {
+		if(connect_fd_row(b, i)) {
+			return true;
+		}
+		i = i + 1;
+	}
+	return false;
+}
+bool connect_diag(Board b) {
+	return connect_forward_diag(b) ; //|| connect_back_diag(b);
+}
+
+bool connect_four(Board b) {
+	return /*connect_across() ||*/ connect_vert(b) || connect_diag(b); 
+}
+
+int main() {
+	Row r(7,' ');
+	Board board(6,r);
+	
+	assert(connect_four(board) == false);
+	
+	return 0;
+}
